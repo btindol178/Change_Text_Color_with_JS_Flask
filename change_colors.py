8314da99568802672f341afac361eb462f3993ba
btindol178 <datamaster178@gmail.com>
2021-12-30 09:55:12 -0500

    Add files via upload

---
+++ change_colors.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('my-form2.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    Number = request.form['Number']
+    processed_text = text.upper()
+    return render_template('my-form2.html',processed_text=processed_text,value = Number)
+
+if __name__ == "__main__":
+    app.run(debug=True)
